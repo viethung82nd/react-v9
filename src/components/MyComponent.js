8f8ff23643583c4296b9e72f46e0e4345fddbbe1
viethung82nd <viethungnguyen204@gmail.com>
2025-06-12 21:00:43 +0700

    video 2.16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>My First Component</div>
+        );
+    }
+}
+
+export default MyComponent;
